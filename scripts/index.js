b68ceb02cc986b56a55888e9061751adc689be7a
malq <1194655645@qq.com>
2015-11-14 09:29:54 +0800

    init

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+window.onload=function(){
+	var el=document.getElementById('test');
+	for(var i=0;i<56;i++){
+		var els=document.createElement('div');
+		els.setAttribute('class','item');
+		// els.setAttribute('id','_'+i);
+		var r=Math.floor(Math.random()*256);
+		var b=Math.floor(Math.random()*256);
+		var g=Math.floor(Math.random()*256);
+	 	els.style.backgroundColor='rgb('+r+','+b+','+g+')';
+	 	if(Math.random()>0.5){
+	 		els.innerHTML=String.fromCharCode(Math.floor(Math.random()*26+65));
+	 	}
+		
+		else{els.innerHTML=String.fromCharCode(Math.floor(Math.random()*26+97));}
+		
+		
+		el.appendChild(els);
+	}
+	
+	// }
+
+		var item=document.getElementsByClassName('item');
+		var i=0;
+		var item1=document.createElement('div');
+		item1.setAttribute('class','item1')
+		el.appendChild(item1);
+		var aa='00';
+		item1.innerHTML='时间';
+		var hasjishuqi=false;
+		
+		document.onkeydown=function(e){
+			if(String.fromCharCode(e.keyCode+32)==item[i].innerHTML&&e.shiftKey==false||
+			String.fromCharCode(e.keyCode)==item[i].innerHTML&&e.shiftKey==true){
+			// el.removeChild(item[i]);
+			item[i].style.backgroundColor='white';
+			i++;
+			
+
+		}
+		if(!hasjishuqi){
+		var t=setInterval(function(){
+			item1.innerHTML='00:'+aa;
+			aa++;
+			
+			},1000);
+		hasjishuqi=true;
+	}
+	};
+	
+		   if(i>=item.length){
+		      // location.reload();
+		      clearInterval(t);
+	}
+	
+			
+
+
+
+}
